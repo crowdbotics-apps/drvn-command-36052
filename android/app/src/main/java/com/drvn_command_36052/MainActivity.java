e7c6aa2055416acc5c93d1d4e6236d8d0d5786c3
Crowdbotics <team@crowdbotics.com>
2022-07-06 02:16:28 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/drvn_command_36052/MainActivity.java
@@ -0,0 +1,15 @@
+package com.drvn_command_36052;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "drvn_command_36052";
+  }
+}
